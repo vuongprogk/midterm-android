6916b9aec33963bc8d78e4be22102857c0825107
ace <vuong753333@gmail.com>
2024-12-01 23:11:22 +0700

    feat: add call

--- app/src/main/java/vn/edu/stu/student/dh52112120/About.java
+++ app/src/main/java/vn/edu/stu/student/dh52112120/About.java
@@ -1,15 +1,18 @@
 package vn.edu.stu.student.dh52112120;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -22,11 +25,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class About extends AppCompatActivity implements OnMapReadyCallback {
-    Button btn_dial, btn_map;
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
+    Button btn_dial, btn_map, btn_call;
     TextView tv_phone;
     MapView mapView;
     GoogleMap mMap;
-    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +58,7 @@ public class About extends AppCompatActivity implements OnMapReadyCallback {
 
     private void addControls() {
         btn_dial = findViewById(R.id.btn_dial);
+        btn_call = findViewById(R.id.btn_call);
         btn_map = findViewById(R.id.btn_map);
         tv_phone = findViewById(R.id.tv_phone);
         mapView = findViewById(R.id.map_about);
@@ -63,6 +67,30 @@ public class About extends AppCompatActivity implements OnMapReadyCallback {
     private void addEvents() {
         btn_dial.setOnClickListener(view -> processDial());
         btn_map.setOnClickListener(view -> processOpenMap());
+        btn_call.setOnClickListener(view -> processCall());
+    }
+
+    private void processCall() {
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 101);
+            }
+            String phone = tv_phone.getText().toString();
+            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + phone));
+            startActivity(intent);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 101) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                processCall();
+            }
+        }
     }
 
     private void processOpenMap() {
@@ -86,6 +114,7 @@ public class About extends AppCompatActivity implements OnMapReadyCallback {
         mMap.addMarker(new MarkerOptions().position(university).title("Marker in Saigon Technology University"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(university));
     }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
